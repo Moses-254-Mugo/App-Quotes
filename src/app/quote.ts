70b26d288c0432c51ae794902979121f700841d3
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-22 21:59:00 +0300

    Add form elements to the page

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
+    public author: string;
+    public name:  string;
+    public description: string;
+
+    showDescription: boolean;
+    constructor (author: string,  name: string, description: string){
+        this.author = author;
+        this.name = name;
+        this.description = description;
+
+    }
 }
