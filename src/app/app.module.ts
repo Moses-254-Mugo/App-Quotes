57432c15e70b69079ed4df3f764d3cd9ecf6ef12
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-22 15:02:59 +0300

    Add home page components to the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
