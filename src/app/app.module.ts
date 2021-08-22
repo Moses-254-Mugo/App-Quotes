aa821d2170e30e1382c8f7cec6e2c05b321069c3
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-22 21:51:00 +0300

    Add component quote-form and class quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    HomepageComponent
+    HomepageComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
